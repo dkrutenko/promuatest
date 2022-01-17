0bac1a8ba1763bdf0e06e2ac9f9c7d520b31b320
Dmytro Krutenko <dospecwork@gmail.com>
2022-01-17 12:05:10 +0200

    update 1

---
+++ src/main/java/brain/com/ua/modals/LoginModal.java
@@ -0,0 +1,48 @@
+package main.java.brain.com.ua.modals;
+
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+import main.java.brain.com.ua.pageobjects.BasePage;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+public class LoginModal extends BasePage<LoginModal> {
+
+    private final SelenideElement phoneField = $x("//input[@class='input-field phone_mask']");
+    private final SelenideElement passwordField = $x("//input[@class='input-field br-login-pass-field']");
+
+    @Step("LoginModal: Filled login fields(phone and password)")
+    public LoginModal fillLoginFields(String phone, String password) {
+        phoneField.click();
+        phoneField.sendKeys(phone);
+        passwordField.click();
+        passwordField.sendKeys(password);
+
+        return this;
+    }
+
+    @Step("LoginModal: Clicked on 'Sign in' button")
+    public LoginModal clickOnSignInButton() {
+        $x("//form[@class='br-login-form modal-br-login-form']//button[@type='submit']").click();
+
+        return this;
+    }
+
+    public String getPhoneEmptyFieldErrorMessage() {
+        var emptyPhoneFieldMsg = $x("//label[@class='error error_msg empty_field'][@for='modal-login-phone-field']").getText();
+
+        return emptyPhoneFieldMsg;
+    }
+
+    public String getPasswordEmptyFieldErrorMessage() {
+        var emptyPasswordFieldMsg = $x("//label[@class='error error_msg empty_field'][@for='modal-login-password-field']").getText();
+
+        return emptyPasswordFieldMsg;
+    }
+
+    public String getLoginErrorMessage() {
+        var loginErrorMsg = $x("//div[@class='login-error']").getText();
+
+        return loginErrorMsg;
+    }
+}
--- src/main/java/brain/com/ua/pageobjects/BasePage.java
+++ src/main/java/brain/com/ua/pageobjects/BasePage.java
@@ -1,5 +1,55 @@
 package main.java.brain.com.ua.pageobjects;
 
+import com.codeborne.selenide.SelenideElement;
+import io.qameta.allure.Step;
+
+import static com.codeborne.selenide.Selenide.$x;
+
 public abstract class BasePage<T extends BasePage<T>> {
 
+    private final SelenideElement search = $x("//div[@class='container container-new']//div[@class='search-form header-search-form']");
+
+    @Step("BasePage: Sended text in search field")
+    public T fillSearchField(String text) {
+        search.click();
+        search.clear();
+        search.sendKeys(text);
+
+        return (T) this;
+    }
+
+    @Step("BasePage: Clicked on 'Знайти' button")
+    public T clickOnSearchButton() {
+        $x("//div[@class='container container-new']//div[@class='search-form header-search-form']//input[@type='submit']").click();
+
+        return (T) this;
+    }
+
+    @Step("BasePage: Clicked on 'Магазини' button ")
+    public T clickShopsButton() {
+        $x("//li[@class='top-menu-list-item folder']").click();
+
+        return (T) this;
+    }
+
+    @Step("BasePage: Clicked on 'Всі категорії' button")
+    public T clickOnAllCategoriesButton() {
+        $x("//li[@class='all']//span").click();
+
+        return (T) this;
+    }
+
+    @Step("BasePage: Clicked on switch language button")
+    public T clickOnSwitchLanguageButton() {
+        $x("//li[@class='header-language-item ']").click();
+
+        return (T) this;
+    }
+
+    @Step("BasePage: Clicked on 'login' button")
+    public T clickOnLoginButton() {
+        $x("//div[@class='br-th-login ']").click();
+
+        return (T) this;
+    }
 }
