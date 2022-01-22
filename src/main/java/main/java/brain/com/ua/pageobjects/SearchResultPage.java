666f15b5e26c809d31de4ce8a96f0d9e609da04e
Dmytro Krutenko <dospecwork@gmail.com>
2022-01-22 14:28:01 +0200

    update 3

---
+++ src/main/java/main/java/brain/com/ua/pageobjects/SearchResultPage.java
@@ -0,0 +1,55 @@
+package main.java.brain.com.ua.pageobjects;
+
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.sizeNotEqual;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+import static java.time.Duration.ofSeconds;
+import static java.util.stream.Collectors.*;
+
+public class SearchResultPage {
+
+    private final ElementsCollection goodsList = $$x("//*[@class='br-pp-desc br-pp-ipd-hidden ']");
+
+    public SearchResultPage selectRequiredCategoryById(String categoryId) {
+        $x("//div[@class='hidden-lg br-sidebar-in']//a[@class='search-category category-"+categoryId+"']")
+                .shouldBe(visible)
+                .click();
+
+        return this;
+    }
+
+    public SearchResultPage clickCategoryButton() {
+        $x("//button[@class='br-o-filter categories']")
+                .shouldBe(visible)
+                .click();
+
+        return this;
+    }
+
+    public String getFirstItemFromList() {
+        return goodsList
+                .shouldBe(sizeNotEqual(0), ofSeconds(10))
+                .stream()
+                .map(SelenideElement::getText)
+                .collect(toList())
+                .stream()
+                .findFirst()
+                .toString();
+    }
+
+    public String getLastItemFromList() {
+        return goodsList
+                .shouldBe(sizeNotEqual(0), ofSeconds(10))
+                .stream()
+                .map(SelenideElement::getText)
+                .collect(toList())
+                .get(goodsList.size() - 1);
+    }
+}
--- src/test/java/brain/com/ua/SearchFormTest.java
+++
@@ -1,6 +0,0 @@
-package brain.com.ua;
-
-import main.java.brain.com.ua.runners.TestRunner;
-
-public class SearchFormTest extends TestRunner {
-}
