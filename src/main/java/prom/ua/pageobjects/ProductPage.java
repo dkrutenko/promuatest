116183c36a52c34d952f6184190ebbb2dc7c4bdb
SlmKrutenko <114851097+SlmKrutenko@users.noreply.github.com>
2022-10-02 13:30:52 +0300

    prom-search // add retrieveItemTitle method

--- src/main/java/prom/ua/pageobjects/ProductPage.java
+++ src/main/java/prom/ua/pageobjects/ProductPage.java
@@ -1,4 +1,13 @@
 package prom.ua.pageobjects;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class ProductPage extends BasePage<ProductPage> {
+    public String retrieveItemTitle() {
+        return $x("//h1[@data-qaid='product_name']")
+                .shouldBe(visible)
+                .getText()
+                .trim();
+    }
 }
