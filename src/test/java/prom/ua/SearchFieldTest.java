f522c69ec08ab02afdd7b94154386ba18919670d
SlmKrutenko <114851097+SlmKrutenko@users.noreply.github.com>
2022-10-02 13:32:56 +0300

    prom-search // update SearchFieldTest

--- src/test/java/prom/ua/SearchFieldTest.java
+++ src/test/java/prom/ua/SearchFieldTest.java
@@ -1,6 +1,7 @@
 package prom.ua;
 
 import prom.ua.pageobjects.HomePage;
+import prom.ua.pageobjects.ProductPage;
 import prom.ua.pageobjects.SearchResultPage;
 import prom.ua.runners.TestRunner;
 import org.testng.annotations.Test;
@@ -11,24 +12,19 @@ public class SearchFieldTest extends TestRunner {
 
     @Test
     public void verifySearchFieldTest() {
-        var requiredItem = "Xiaomi Poco";
-        var categoryId = "298 ";
+        var requiredItem = "рюкзак";
+        var actual = "Рюкзак похідний туристичний 80 літрів";
+        var item_id = "1";
         var homePage = new HomePage();
+        var productPage = new ProductPage();
 
         homePage.fillSearchField(requiredItem)
                 .clickOnSearchButton()
-                .clickCategoryButton()
-                .selectRequiredCategoryById(categoryId);
+                .selectFirstItem(item_id)
+                .retrieveItemTitle();
 
-        var firstItem = new SearchResultPage().getFirstItemFromList();
-        var lastItem = new SearchResultPage().getLastItemFromList();
-
-        assertThat(firstItem)
-                .as("Test failed: first item should have 'xiaomi poco' in title")
-                .contains(requiredItem);
-
-        assertThat(lastItem)
-                .as("Test failed: last item should have 'xiaomi poco' in title")
-                .contains(requiredItem);
+        assertThat(actual)
+                .as("Test failed: first item should have " + actual + " in title")
+                .isEqualTo(productPage.retrieveItemTitle());
     }
 }
