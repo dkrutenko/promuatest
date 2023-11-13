e7bd9f53f33a504a86da20b854973f32c4007e74
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2023-11-13 16:16:39 +0200

    update testrunner

--- src/main/java/prom/ua/runners/TestRunner.java
+++ src/main/java/prom/ua/runners/TestRunner.java
@@ -1,16 +1,38 @@
 package prom.ua.runners;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
+
+import java.io.File;
 
 import static com.codeborne.selenide.Configuration.*;
 import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 
 public abstract class TestRunner {
+    WebDriver driver;
+    ChromeDriverService chrome_service;
+
+    @Parameters("browser")
+    public void setupBrowserPath() {
+        String driver_path = "/promuatest/drivers/chromedriver.exe";
+        chrome_service = new ChromeDriverService.Builder().usingDriverExecutable(new File(driver_path)).build();
+    }
 
     @BeforeMethod
     public void openHomePage() {
+        setupBrowserPath();
         timeout = 10000;
+
         open("https://prom.ua/ua/");
+        getWebDriver().manage().window().maximize();
     }
 
+    @AfterMethod
+    public void browserClose() {
+        getWebDriver().quit();
+    }
 }
