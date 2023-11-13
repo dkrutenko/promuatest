3177509c448ef332d90779ee49e856c580534580
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2023-11-13 16:17:46 +0200

    removed not needed libs and methods

--- src/main/java/prom/ua/pageobjects/SearchResultPage.java
+++ src/main/java/prom/ua/pageobjects/SearchResultPage.java
@@ -1,14 +1,11 @@
 package prom.ua.pageobjects;
 
 import com.codeborne.selenide.ElementsCollection;
-import com.codeborne.selenide.SelenideElement;
 
-import static com.codeborne.selenide.CollectionCondition.sizeNotEqual;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$$x;
 import static com.codeborne.selenide.Selenide.$x;
-import static java.time.Duration.ofSeconds;
-import static java.util.stream.Collectors.*;
+
 
 public class SearchResultPage {
 
@@ -36,25 +33,5 @@ public class SearchResultPage {
                 .click();
 
         return this;
-    }
-
-    public String getFirstItemFromList() {
-        return goodsList
-                .shouldBe(sizeNotEqual(0), ofSeconds(10))
-                .stream()
-                .map(SelenideElement::getText)
-                .collect(toList())
-                .stream()
-                .findFirst()
-                .toString();
-    }
-
-    public String getLastItemFromList() {
-        return goodsList
-                .shouldBe(sizeNotEqual(0), ofSeconds(10))
-                .stream()
-                .map(SelenideElement::getText)
-                .collect(toList())
-                .get(goodsList.size() - 1);
     }
 }
