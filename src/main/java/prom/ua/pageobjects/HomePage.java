1e18d6cceda1b151ec50f54ccebb079098ef4c79
Dmytro Krutenko <102244367+DKrutenk0@users.noreply.github.com>
2023-11-13 17:46:20 +0200

    update homepage

--- src/main/java/prom/ua/pageobjects/HomePage.java
+++ src/main/java/prom/ua/pageobjects/HomePage.java
@@ -1,4 +1,20 @@
 package prom.ua.pageobjects;
 
+import io.qameta.allure.Step;
+
+import java.util.Random;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
 public class HomePage extends BasePage<HomePage> {
+    private static Random random = new Random();
+
+    @Step("HomePage: Select first available 'for you' item on homepage")
+    public void selectRandomForYouItem() {
+        $x("//span[text()='Для тебе']").scrollIntoView(("{behavior: \"instant\", block: \"center\", inline: \"center\"}"));
+        var for_you_items_list = $$x("//div[@class='M3v0L YKUY6']/following-sibling::div/div");
+        int random_index = random.nextInt(for_you_items_list.size());
+        for_you_items_list.get(random_index).click();
+    }
 }
